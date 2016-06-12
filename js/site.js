4233a6e4420b3d64d2095697c68d180eb9e8f8fe
Dylan <fatherparkicism@hotmail.com>
2016-06-12 16:07:57 -0400

    basic hello world

---
+++ js/site.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {Router, Route, IndexRoute, browserHsitory} from "react-router";
+
+const app = document.getElementById("app");
+
+ReactDOM.render(
+  <p> Hello World </p>,
+  app
+);
