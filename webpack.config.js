7eea9cf37d186d36203fb2607ca41579e807e918
Dylan <fatherparkicism@hotmail.com>
2016-06-12 15:53:26 -0400

    edit gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-  context: path.join(__dirname, "src"),
+  context: path.join(__dirname),
   devtool: debug ? "inline-sourcemap" : null,
   entry: "./js/site.js",
   module: {
@@ -20,7 +20,7 @@ module.exports = {
     ]
   },
   output: {
-    path: __dirname + "/src/",
+    path: __dirname,
     filename: "site.min.js"
   },
   plugins: debug ? [] : [
