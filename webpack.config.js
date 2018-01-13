eb2bf206ceb1115c5ef4060b10a742ce256b6492
SANGMYUNG PARK <sm5park@uwaterloo.ca>
2018-01-13 16:00:18 -0500

    webpack configuration changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-const debug = process.env.NODE_ENV !== 'production';
 const webpack = require('webpack');
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
@@ -7,55 +6,57 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 let extractCss = new ExtractTextPlugin('css/style.css');
 let generateHtml = new HtmlWebpackPlugin({ template: 'src/index.html' });
 
-module.exports = {
-  node: {
-    fs: 'empty'
-  },
-  devtool: debug ? "inline-sourcemap" : null,
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname),
-    filename: "bundle.js"
-  },
-  resolve: {
-    modules: [
-      path.resolve(__dirname, 'src'),
-      path.resolve(__dirname, 'node_modules'),
-      path.resolve(__dirname, 'src', 'page')
-    ]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js|\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        include: [
-          path.join( __dirname, 'src')
-        ],
-        use: ['babel-loader']
-      },
-      {
-        test: /\.scss$/,
-        use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
-          fallback: [{loader: 'style-loader'}],
-          use: ['css-loader', 'sass-loader']
-        }))
-      }
-    ]
-  },
-  plugins: debug ? [
-    extractCss,
-    generateHtml
-  ] : [
-    extractCss,
-    generateHtml,
-    new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify("production")
-      }
-    }),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-  ],
-};
+module.exports = (env) => {
+  const debug = !env.production;  
+  return {
+    node: {
+      fs: 'empty'
+    },
+    devtool: "inline-sourcemap",
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname),
+      filename: "bundle.js"
+    },
+    resolve: {
+      modules: [
+        path.resolve(__dirname, 'src'),
+        path.resolve(__dirname, 'node_modules'),
+        path.resolve(__dirname, 'src', 'page')
+      ]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js|\.jsx?$/,
+          exclude: /(node_modules|bower_components)/,
+          include: [
+            path.join( __dirname, 'src')
+          ],
+          use: ['babel-loader']
+        },
+        {
+          test: /\.scss$/,
+          use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
+            fallback: [{loader: 'style-loader'}],
+            use: ['css-loader', 'sass-loader']
+          }))
+        }
+      ]
+    },
+    plugins: debug ? [
+      extractCss,
+      generateHtml
+    ] : [
+      extractCss,
+      generateHtml,
+      new webpack.DefinePlugin({
+        "process.env": {
+          NODE_ENV: JSON.stringify("production")
+        }
+      }),
+      new webpack.optimize.OccurrenceOrderPlugin(),
+      new webpack.optimize.UglifyJsPlugin({ mangle: true, sourcemap: false }),
+    ],
+  };
+}
